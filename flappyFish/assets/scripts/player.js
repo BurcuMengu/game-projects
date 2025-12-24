a94cd6514aefa303f37782132ab73658493b1b45
BurcuMengu <burcumengu@gmail.com>
2025-12-24 15:14:08 +0300

    Add flap mechanic with scaled vertical speed and boundary collision chechs

--- flappyFish/assets/scripts/player.js
+++ flappyFish/assets/scripts/player.js
@@ -8,6 +8,7 @@ class Player {
         this.width;
         this.height;
         this.speedY;
+        this.flapSpeed;
     }
     draw() {
         this.game.ctx.fillRect(this.x, this.y, this.width, this.height);
@@ -28,8 +29,17 @@ class Player {
         this.height = this.spriteHeight * this.game.ratio;
         this.y = this.game.height * 0.5 - this.height * 0.5;
         this.speedY = -8 * this.game.ratio;
+        this.flapSpeed = 5 * this.game.ratio;
+    }
+    isTouchinTop() {
+        return this.y <= 0;
     }
     isTouchingBotton() {
         return this.y >= this.game.height - this.height;
+    }
+    flap() {
+        if(!this.isTouchinTop()) {
+            this.speedY = -this.flapSpeed;
+        }
     }
 }
